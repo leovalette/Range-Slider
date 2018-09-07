4efc04902e139029bd49496765547bf51a6bae24
Quentin Fombaron <q.fombaron@hotmail.fr>
2018-09-07 11:28:53 +0200

    Add RangeSlider Interface + Class

---
+++ src/ihm/rangeslider/RangeSlider.java
@@ -0,0 +1,60 @@
+package ihm.rangeslider;
+
+public class RangeSlider implements _RangeSlider{
+    private int MaximumCursor;
+    private int MinimumCursor;
+    private int Maximum;
+    private int Minimum;
+
+    public RangeSlider(int maxCursor, int minCursor, int max, int min) {
+        this.MaximumCursor = maxCursor;
+        this.MinimumCursor = minCursor;
+        this.Maximum = max;
+        this.Minimum = min;
+    }
+
+    @Override
+    public int getMinimumCursor() {
+        return this.MinimumCursor;
+    }
+
+    @Override
+    public int getMaximumCursor() {
+        return this.MaximumCursor;
+    }
+
+    @Override
+    public void setMinimumCursor(int min) {
+        this.MinimumCursor = min;
+    }
+
+    @Override
+    public void setMaximumCursor(int max) {
+        this.MaximumCursor = max;
+    }
+
+    @Override
+    public int getMinimum() {
+        return this.Minimum;
+    }
+
+    @Override
+    public int getMaximum() {
+        return this.Maximum;;
+    }
+
+    @Override
+    public void setMinimum(int min) {
+        this.Minimum = min;
+    }
+
+    @Override
+    public void setMaximum(int max) {
+        this.Maximum = max;
+    }
+
+    @Override
+    public boolean checkValues() {
+        return getMinimumCursor() < getMaximumCursor();
+    }
+}
---
+++ src/ihm/rangeslider/_RangeSlider.java
@@ -0,0 +1,16 @@
+package ihm.rangeslider;
+
+public interface _RangeSlider {
+
+    int getMinimumCursor();
+    int getMaximumCursor();
+    void setMinimumCursor(int min);
+    void setMaximumCursor(int max);
+
+    int getMinimum();
+    int getMaximum();
+    void setMinimum(int min);
+    void setMaximum(int max);
+
+    boolean checkValues();
+}
