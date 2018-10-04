5a59993841e9877ba20b7936002df66235c38463
Léo <Lo@DESKTOP-C0S8M4T.mshome.net>
2018-10-04 15:08:26 +0200

    Add conditions and display distance circle on screen

--- src/ihm/rangeslider/Points.java
+++ src/ihm/rangeslider/Points.java
@@ -14,17 +14,26 @@ public class Points extends JPanel {
 	 */
 	private static final long serialVersionUID = 3746104870971787036L;
 	private List<Home> homeList = new ArrayList<Home>();
+	private int size;
+	private int dist;
 
-	public Points(List<Home> homeList) throws HeadlessException {
+	public Points(List<Home> homeList, int size, int distMax) throws HeadlessException {
 		this.homeList = homeList;
+		this.size = size;
+		this.dist = distMax;
 	}
 
-	public void paintComponent(Graphics g) { 
-		
+	public void paintComponent(Graphics g) {
+
+		g.drawLine(size/2 - 10, size/2 - 10, size/2 + 10, size/2 + 10);
+		g.drawLine(size/2 - 10, size/2 + 10, size/2 + 10, size/2 - 10);
+
+		g.drawOval(250-dist, 250-dist, 2 * dist, 2 * dist);
+
 		for (int i = 0; i < homeList.size(); i++) {
-			g.fillOval(homeList.get(i).getCoordX(), homeList.get(i).getCoordY() , 10, 10);
+			g.fillOval(homeList.get(i).getCoordX() - 5, homeList.get(i).getCoordY() - 5, 10, 10);
 		}
-		
+
 	}
 
 }
